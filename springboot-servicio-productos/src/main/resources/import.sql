af614735457813d6d93a149f75a031445e3989a8
cesarvargas1990 <ceavarga@bancolombia.com.co>
2022-06-29 11:23:16 -0500

    Primer commit

---
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into productos (nombre,precio,create_at) values ('Telefono', 3200000, NOW());
+insert into productos (nombre,precio,create_at) values ('Computador', 2500000, NOW());
+insert into productos (nombre,precio,create_at) values ('Mesa', 700000, NOW());
+insert into productos (nombre,precio,create_at) values ('Freidora de aire', 800000, NOW());
+insert into productos (nombre,precio,create_at) values ('Manguera', 85000, NOW());
+insert into productos (nombre,precio,create_at) values ('Estufa', 450000, NOW());
